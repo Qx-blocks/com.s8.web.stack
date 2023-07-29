0cc3d3f33fdd707b282376d67e89ae12412720c5
Pierre Convert <pierreconvert@macbook-pro-6.home>
2023-07-29 13:01:07 +0200

    new features added

--- sources/com/s8/web/front/carbide/objform/EnumObjFormSetter.java
+++ sources/com/s8/web/front/carbide/objform/EnumObjFormSetter.java
@@ -1,5 +1,7 @@
 package com.s8.web.front.carbide.objform;
 
+import java.util.function.Consumer;
+
 import com.s8.io.bohr.neon.core.NeBranch;
 import com.s8.io.bohr.neon.functions.primitives.UInt8NeFunction;
 import com.s8.io.bohr.neon.lambdas.primitives.UInt8Lambda;
@@ -12,6 +14,22 @@ import com.s8.io.bohr.neon.lambdas.primitives.UInt8Lambda;
  */
 public class EnumObjFormSetter extends PrimitiveObjFormSetter {
 	
+	
+	public static <E extends Enum<E>> EnumObjFormSetter create(NeBranch branch, String name, Class<E> enumType, int preset, 
+			Consumer<E> consumer, String doc) {
+		
+		E[] enumValues = enumType.getEnumConstants();
+		int n = enumValues.length;
+		String[] enumNames = new String[n];
+		for(int i = 0; i<n; i++) { enumNames[i] = enumValues[i].name(); }
+		EnumObjFormSetter formSetter = new EnumObjFormSetter(branch);
+		formSetter.setName(name);
+		formSetter.setEnumValues(enumNames);
+		formSetter.setPresetIndex(preset);
+		formSetter.onSelectedLambda(index -> consumer.accept(enumValues[index]));
+		formSetter.setTooltipDoc(doc);
+		return formSetter;
+	}
 
 	
 	public EnumObjFormSetter(NeBranch branch) {
